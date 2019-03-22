f29bedd3c137355996cd5110c3587ce987353664
dandraig <48811687+dandraig@users.noreply.github.com>
2019-03-22 04:45:24 -0300

    Add files via upload

---
+++ navio/src/batalhanaval/batalhanaval.java
@@ -0,0 +1,25 @@
+package batalhanaval;
+//mapa/tabuleiro 48x48
+public class batalhanaval {
+	public static void main(String[] args) { 
+	
+	String mapa[][]= new String[48][48];
+	
+	int posicao[][]  = new int[48][48];
+	
+       private static void zeraPosicao() {
+		         for(int l=0;l<48;l++);
+                 for(int c=0;c<48;c++);
+                      int[][] posicao;
+					posicao[l][c] = 0;
+				
+}
+    public class Navio{ 
+    char nomedoplayer;
+    float hp;
+    float mover; 
+    }
+    
+	}
+		
+	
